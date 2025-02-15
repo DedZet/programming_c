04f55a9a0b608382d0d15178635a3128bfbba5fe
DedZet <51733299+DedZet@users.noreply.github.com>
2025-02-15 13:00:59 +1000

    Update main.c

--- labs/lab1/main.c
+++ labs/lab1/main.c
@@ -2,21 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 int main(int argc, char *argv[]) {
 	float a, b, c, dm, x1, x2, d;
 	
 	a = 1;
 	b = 5;
 	c = 3;
-	
 	d = pow(b,2)-4*a*c; // pow - ñòåïåíü
 	
+	if (d < 0) 
+	{
+		printf("D = %lf\n", d);
+		printf("Kornei net"); 
+	}
+////////////////////////////////////////////////
+	else if (d == 0) 
+	{
+		x1 = -b /(2*a);
+		printf("D = %lf\n", d);
+		printf("x1 = %lf\n", x1);
+	}
+////////////////////////////////////////////////
+	else 
+	{
 	x1 = -b + sqrt(d)/(2*a);
 	x1 = -b - sqrt(d)/(2*a);
 
 	printf("D = %lf\n", d);
 	printf("x1 = %lf\n", x1);
 	printf("x2 = %lf\n", x2);
+		
+	}
 	
 	return 0;
 }
