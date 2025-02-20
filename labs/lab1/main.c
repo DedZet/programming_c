034ad2aba550bcf3a1a268530f371ff1c9f6e00a
DedZet <51733299+DedZet@users.noreply.github.com>
2025-02-20 20:17:33 +1000

    Update main.c

--- labs/lab1/main.c
+++ labs/lab1/main.c
@@ -2,46 +2,71 @@
 #include <stdlib.h>
 #include <math.h>
 
+//void continue() {
+//	printf("Continue? y/n\n");
+//	scanf("\n%c,", &resp);
+//}
+	
 
-int main(int argc, char *argv[]) {
-	double a, b, c, dm, x1, x2, d;
+int main() {
+	double a, b, c, x1, x2, d;
 	
-	printf("Enter A: ");
-	scanf("%lf,", &a);
+	char resp = 'y';
 	
-	printf("Enter B: ");
-	scanf("%lf,", &b);
+	while (resp == 'y') {
 	
-	printf("Enter C: ");
-	scanf("%lf,", &c);
+		printf("\nEnter A: ");
+		scanf("%lf,", &a);
+	
+		printf("Enter B: ");
+		scanf("%lf,", &b);
+	
+		printf("Enter C: ");
+		scanf("%lf,", &c);
 
-	d = pow(b,2)-4*a*c; // pow - ñòåïåíü
+		d = pow(b,2)-4*a*c; // pow - ñòåïåíü
 	
-	printf("\n");
+		printf("\n");
 	
-	if (d < 0) 
-	{
-		printf("D = %lf\n", d);
-		printf("Kornei net"); 
-	}
-////////////////////////////////////////////////
-	else if (d == 0) 
-	{
-		x1 = -b /(2*a);
+		if (d < 0) 
+		{
+			printf("D = %lf\n", d);
+			printf("Kornei net\n");
+			
+			printf("Continue? y/n\n");
+			scanf("\n%c,", &resp);
+			
+			
+			
+		}
+
+		else if (d == 0) 
+		{
+			x1 = -b /(2*a);
+			printf("D = %lf\n", d);
+			printf("x1 = %lf\n", x1);
+			
+			printf("Continue? y/n\n");
+			scanf("\n%c,", &resp);
+			
+		}
+		
+		else 
+		
+		{
+		x1 = -b + sqrt(d)/(2*a);
+		x1 = -b - sqrt(d)/(2*a);
+
 		printf("D = %lf\n", d);
 		printf("x1 = %lf\n", x1);
-	}
-////////////////////////////////////////////////
-	else 
-	{
-	x1 = -b + sqrt(d)/(2*a);
-	x1 = -b - sqrt(d)/(2*a);
-
-	printf("D = %lf\n", d);
-	printf("x1 = %lf\n", x1);
-	printf("x2 = %lf\n", x2);
+		printf("x2 = %lf\n", x2);
 		
+		printf("Continue? y/n\n");
+		scanf("\n%c,", &resp);
+		
+		}
 	}
 	
 	return 0;
 }
+
