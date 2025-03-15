40143e3a99aced8badd89d715d73ed4b4336305e
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-15 13:15:38 +1000

    Update main2.c

--- labs/lab2/main2.c
+++ labs/lab2/main2.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
 	double mat[2][2];
 	for (int i = 0; i < 2; i++) {
 	    for (int j = 0; j < 2; j++) {
-        	scanf("%lf", &x[i][j]);
+        	scanf("%lf", &mat[i][j]);
 	    }
     }
 
@@ -22,10 +22,23 @@ int main(int argc, char *argv[]) {
 ////////////////////////////////////////////////////
     printf("\nMATRIX^2:\n");
     
-    int mul[2][2] = {
-        { mat[0][0]*mat[0][0] + mat[0][1]*mat[1][0], mat[0][0]*mat[0][1] + mat[0][1]*mat[1][1] },
-        { mat[1][0]*mat[0][0] + mat[1][1]*mat[1][0], mat[1][0]*mat[0][1] + mat[1][1]*mat[1][1] },
-    };
+    // int mul[2][2] = {
+    //     { mat[0][0]*mat[0][0] + mat[0][1]*mat[1][0], mat[0][0]*mat[0][1] + mat[0][1]*mat[1][1] },
+    //     { mat[1][0]*mat[0][0] + mat[1][1]*mat[1][0], mat[1][0]*mat[0][1] + mat[1][1]*mat[1][1] },
+    // };
+    
+    int mul[2][2];
+	for (int i = 0; i < 2; i++) {
+	    for (int j = 0; j < 2; j++) {
+	        mul[i][j] = 0;
+	        
+	        for (int k = 0; k < 2; k++)
+        	mul[i][j] += mat[i][k]*mat[k][j];
+	    }
+    }
+    
+    // 11 14
+    // 35 46
     
 	for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
