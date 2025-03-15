ff5d407e7c2ff0ee29c1fb02f740a80c63b8ba9f
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-15 12:22:37 +1000

    Create main.c

---
+++ labs/lab5/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+void printarr(double arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%f ", arr[i]);
+    }
+}
+
+void typechoose() {
+    printf("Matrix / Array");
+    if (scanf("%c",)) {
+        
+    }
+    
+}
+
+int main() {
+    
+    double x1,x2,x3,x4,x5,x6,x7,x8,x9;
+    
+    printf("First row \n");
+    scanf("%lf %lf %lf", &x1, &x2, &x3);
+    
+    printf("Second row \n");
+    scanf("%lf %lf %lf", &x4, &x5, &x6);
+    
+    printf("Third row \n");
+    scanf("%lf %lf %lf", &x7, &x8, &x9);
+
+	double matrix1[3][3] = {
+		{x1,x2,x3}, 
+		{x4,x5,x6}, 
+		{x7,x8,x9}}; 
+		
+	double matrix2[3][3] = {
+        {1,5,3},
+        {4,4,4},
+        {3,2,1}};
+    
+    double array1[] = {1,2,3,4,3};
+    double array2[] = {1,6,5,4,3};
+
+    
+        
+    printarr(matrix1,5);
+    
+    
+    return 0;
+}
