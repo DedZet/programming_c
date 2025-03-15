a7e2488a21ba89813e851e9ab7713a5d55fbf8c8
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-15 12:54:16 +1000

    Update main.c

--- labs/lab2/main.c
+++ labs/lab2/main.c
@@ -2,37 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    // double x1,x2,x3,x4,x5,x6,x7,x8,x9;
-    
- //    printf("First row \n");
- //    scanf("%lf %lf %lf", &x1, &x2, &x3);
-    
- //    printf("Second row \n");
- //    scanf("%lf %lf %lf", &x4, &x5, &x6);
-    
- //    printf("Third row \n");
- //    scanf("%lf %lf %lf", &x7, &x8, &x9);
 
-	// double mat[3][3] = {
-	// 	{x1,x2,x3}, //6
-	// 	{x4,x5,x6}, // 15
-	// 	{x7,x8,x9}}; // 24 
-
-int x[9];
+    double x[9];
     for (int i = 0; i < 9; i++) {
         scanf("%lf", &x[i]);
     
     }
 
-	double matrix1[3][3] = {
-		{x[0],x[1],x[2]}, 
+	double mat[3][3] = {
+		{x[0],x[1],x[2]},
 		{x[3],x[4],x[5]}, 
 		{x[6],x[7],x[8]}}; 
-		
-	double matrix2[3][3] = {
-        {1,5,3},
-        {4,4,4},
-        {3,2,1}};
     
 ////////////////////////////////////////////////////
 	printf("\nMATRIX:\n");
