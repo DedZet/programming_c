382b9debf86b89a0108414f80b9a94a7f3b7503f
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-16 21:45:24 +1000

    Update lab0.c

--- vas labs/lab0.c
+++ vas labs/lab0.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool isprime(int n) {
+    int flag = 0;
+    
+    if (n == 0 || n == 1) {
+        flag = 1;
+    }
+    
+    for (int i = 2; i <= n / 2; i++) {
+        if ((n % i) == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    
+    if (flag == 0) {
+        return true;
+    }
+    
+    else {return false;}
+}
+
 
 int main() {
     
-    int P[] = {1,2,3,4,5,6};
+    int P[] = {1,2,3,4,5,6,7};
     int p = 2;
     
-    for (int i = 0; i < sizeof(P); i++) {
-        if ((P[i] % p) == 0) {
-            printf("\n%i", P[i]);
-        }
+    int len = sizeof(P) / sizeof(P[0]);
+    
+    if (isprime(4)) {
+        printf("True");
     }
+    else {printf("False");}
+
+    // for (int i = 0; i < len; i++) {
+    //     if (isprime(P[i])) {
+    //         printf("\n%i", P[i], "\n");
+    //     }
+    // }
     
     return 0;
 }
