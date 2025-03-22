069dfacd0b59bb716fa2d23f124924dc92f7f62c
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-22 12:36:42 +1000

    Update main2.c

--- labs/lab2/main2.c
+++ labs/lab2/main2.c
@@ -2,51 +2,49 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    
+    int scale;
+    printf("Enter sq scale\n");
+    scanf("%i", &scale);
 
-	double mat[2][2];
-	for (int i = 0; i < 2; i++) {
-	    for (int j = 0; j < 2; j++) {
+	double mat[scale][scale];
+	for (int i = 0; i < scale; i++) {
+	    for (int j = 0; j < scale; j++) {
         	scanf("%lf", &mat[i][j]);
 	    }
     }
 
 ////////////////////////////////////////////////////
+
 	printf("\nMATRIX:\n");
-	for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+	for (int i = 0; i < scale; i++) {
+        for (int j = 0; j < scale; j++) {
             printf("%lf\t", mat[i][j], "/n");
         }
         printf("\n");
 	} printf("\n");
     
 ////////////////////////////////////////////////////
+
     printf("\nMATRIX^2:\n");
     
-    // int mul[2][2] = {
-    //     { mat[0][0]*mat[0][0] + mat[0][1]*mat[1][0], mat[0][0]*mat[0][1] + mat[0][1]*mat[1][1] },
-    //     { mat[1][0]*mat[0][0] + mat[1][1]*mat[1][0], mat[1][0]*mat[0][1] + mat[1][1]*mat[1][1] },
-    // };
-    
-    int mul[2][2];
-	for (int i = 0; i < 2; i++) {
-	    for (int j = 0; j < 2; j++) {
+    int mul[scale][scale];
+	for (int i = 0; i < scale; i++) {
+	    for (int j = 0; j < scale; j++) {
 	        mul[i][j] = 0;
 	        
-	        for (int k = 0; k < 2; k++)
+	        for (int k = 0; k < scale; k++)
         	mul[i][j] += mat[i][k]*mat[k][j];
 	    }
     }
     
-    // 11 14
-    // 35 46
-    
-	for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+	for (int i = 0; i < scale; i++) {
+        for (int j = 0; j < scale; j++) {
             printf("%i\t", mul[i][j], "/n");
         }
         printf("\n");
 	} 
 	printf("\n");
-
+	
 	return 0;
 }
