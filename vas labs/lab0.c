d57368efe69282ffb8465d57479b10fe6d8a4b8f
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-23 16:12:46 +1000

    Update lab0.c

--- vas labs/lab0.c
+++ vas labs/lab0.c
@@ -4,25 +4,25 @@
 
 void erat(int n) {
 
-    bool *isPrime = (bool *)malloc((n + 1) * sizeof(bool));
+    bool *isPrime = (bool *)malloc((n + 1) * sizeof(bool)); // Выделение динамической памяти для bool списка, создание списка
     for (int i = 0; i <= n; i++) {
         isPrime[i] = true; 
-    }
+    } // заполнение списка значениями true
 
     for (int p = 2; p * p <= n; p++) {
         if (isPrime[p] == true) {
             for (int i = p * p; i <= n; i += p) {
                 isPrime[i] = false;
-            }
+            } // обработка простых чисел, замена элементов списка на false
         }
     }
     
     for (int p = 2; p <= n; p++) {
         if (isPrime[p]) {
             printf("%d ", p);
-        }
+        } // вывод простых чисел в консоль
     }
-    free(isPrime);
+    free(isPrime); // освобождение памяти, удаление списка
 }
 
 int main() {
