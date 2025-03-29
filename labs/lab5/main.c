dbf0989be2fca50ed14db4758af0051a794a3923
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-29 12:25:29 +1000

    Update main.c

--- labs/lab5/main.c
+++ labs/lab5/main.c
@@ -1,32 +1,119 @@
 #include <stdio.h>
 
 void printMatrix(int row, int col, double m[row][col]) {
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++) {
+	int i;
+    for(i=0; i<row; i++){
+    	int j;
+        for(j=0; j<col; j++) {
            printf("%lf ", m[i][j]); 
         }
         printf("\n");
     }
 }
 
-double* makemat(int r, int c) {
+void makemat(char text[2]) {
 
-    double* ptr = malloc((r * c) * sizeof(double));
-    for (int i = 0; i < r * c; i++) {
-        ptr[i] = i + 1;
-    }
-    
-    return ptr;
 }
 
 int main() {
     
-    int r,c;
-    printf("enter rows");
+    
+    
+	int r,c;
+    printf("enter rows for M1: ");
     scanf("%i", &r);
+    printf("enter columns for M1: ");
     scanf("%i", &c);
+    double mat[r][c];
+    
+    int i,j;
+    for (i = 0; i < r; i++) {
+	for (j = 0; j < c; j++) {
+        	scanf("%lf", &mat[i][j]);
+		}
+    }
+    
+    //////////////////////////////////////
+    system("cls");
+    
+    int r2,c2;
+    printf("enter rows for M2: ");
+    scanf("%i", &r2);
+    printf("enter columns for M2: ");
+    scanf("%i", &c2);
+    double mat2[r2][c2];
+    
+    int k,l;
+    for (k = 0; k < r2; k++) {
+	for (l = 0; l < c2; l++) {
+        	scanf("%lf", &mat2[k][l]);
+		}
+    }
+    
+	system("cls");
+    printMatrix(r,c,mat);
+    printf("\n");
+	printMatrix(r2,c2,mat2);
 
-    printMatrix(r,c,makemat(r,c));
+	char op;
+	printf("\nenter operation: ");
+    scanf(" %c", &op); // Прабел паставел штоб не сщитывать enter
 
+	switch (op) {
+		case '+':
+			if (r==r2 && c==c2) { // Сложение матриц
+				double suma[r][c];
+
+				int i,j;
+	 			for (i = 0; i < r; i++) {
+        			for (j = 0; j < c; j++) {
+            			suma[i][j] = mat[i][j] + mat2[i][j];
+        				}
+				}
+				
+				printMatrix(r,c,suma);
+			}
+			else {
+				printf("Matrix size error");
+			}
+			free(op);
+			break;
+			
+		case '-':
+			if (r==r2, c==c2) { // Вычитание матриц
+				double sub[r][c];
+
+				int i,j;
+	 			for (i = 0; i < r; i++) {
+        			for (j = 0; j < c; j++) {
+            			sub[i][j] = mat[i][j] - mat2[i][j];
+        				}
+				}
+				
+				printMatrix(r,c,sub);
+			}
+			else {
+				printf("Matrix size error");
+			}
+			break;
+			
+		case '*':
+			printf("*"); // патом дапишу
+//			double mul[r][c];
+//
+//				int i,j;
+//	 			for (i = 0; i < r; i++) {
+//        			for (j = 0; j < c; j++) {
+//            			mul[i][j] = mat[i][j] + mat2[i][j];
+//        				}
+//				}
+				
+				//printMatrix(r,c,mul);
+			break;
+			
+		default:
+			printf("Not an operation\n");
+	
     return 0;
+	}
 }
