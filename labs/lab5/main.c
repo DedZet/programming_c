18b303bed57fd9af45ffcd65d7928e1d0f21dddc
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-30 14:32:28 +1000

    Update main.c

--- labs/lab5/main.c
+++ labs/lab5/main.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-
-void printMatrix(int row, int col, double m[row][col]) {
-	int i;
-    for(i=0; i<row; i++){
-    	int j;
-        for(j=0; j<col; j++) {
-           printf("%lf ", m[i][j]); 
-        }
-        printf("\n");
-    }
-}
+#include "matrix.h"
 
 int main() {
     
@@ -56,69 +45,8 @@ int main() {
 	printf("\nenter operation: ");
     scanf(" %c", &op);
 
-	switch (op) {
-		case '+':
-			if (r==r2 && c==c2) { 
-				double suma[r][c];
-
-				int i,j;
-	 			for (i = 0; i < r; i++) {
-        			for (j = 0; j < c; j++) {
-            			suma[i][j] = mat[i][j] + mat2[i][j];
-        				}
-				}
-				
-				printMatrix(r,c,suma);
-			}
-			else {
-				printf("Matrix size error");
-			}
-			break;
-			
-		case '-':
-			if (r==r2 && c==c2) {
-				double sub[r][c];
-
-				int i,j;
-	 			for (i = 0; i < r; i++) {
-        			for (j = 0; j < c; j++) {
-            			sub[i][j] = mat[i][j] - mat2[i][j];
-        				}
-				}
-				
-				printMatrix(r,c,sub);
-			}
-			else {
-				printf("Matrix size error");
-			}
-			break;
-			
-		case '*':
-			if ((r==r2 && c==c2) || (r2==c)) {
-				double mul[r2][c2];
-				int i,j,k = 0;
-
-				for (i = 0; i < r2; i++) {
-	    			for (j = 0; j < c2; j++) {
-	        			mul[i][j] = 0;
-	        
-	        			for (k = 0; k < r2; k++) {
-        					mul[i][j] += mat[i][k]*mat2[k][j];
-        				}
-	    			}
-    			}
-				
-				printMatrix(r,c2,mul);
-			}
-			else {
-				printf("Matrix size error");
-			}
-			break;
-			
-		default:
-			printf("Not an operation\n");
-			free(op);
+	matrix_op(r,c,r2,c2,mat,mat2,op);
 	
     return 0;
+    
 	}
-}
