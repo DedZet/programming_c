c3bebbb02ad6871e3cabd7ff1fd0ecc081bdb482
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-31 17:05:29 +1000

    Update matrix.c

--- labs/lab5/matrix.c
+++ labs/lab5/matrix.c
@@ -2,34 +2,48 @@
 #include <stdio.h>
 #include "matrix.h"
 
-void printMatrix(int row, int col, double m[row][col]) {
+void printMatrix(int row, int col, double** m) {
 	int i;
     for(i=0; i<row; i++){
     	int j;
         for(j=0; j<col; j++) {
-           printf("%lf ", m[i][j]); 
+           printf("%lf ", m[i][j]);
         }
         printf("\n");
     }
 }
 
-void matrix_op(int r, int c, int r2, int c2, double mat[r][c], double mat2[r2][c2], char op) {
+void free_matrix(double** matrix, int n) {
+	int i;
+    for (i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+
+void matrix_op(int r, int c, int r2, int c2, double** mat, double** mat2, char op) {
 	
-    double** result = (double**)malloc(n * sizeof(double*));
+    double** result = (double**)malloc(r2 * sizeof(double*));
+    
+    int i,j;
+    for (i = 0; i < r2; i++) {
+    		result[i] = (double*)malloc(r2 * sizeof(double));
+		}
+    
     
     switch (op) {
 		case '+':
 			if (r==r2 && c==c2) { 
-				double suma[r][c];
 
 				int i,j;
 	 			for (i = 0; i < r; i++) {
         			for (j = 0; j < c; j++) {
-            			suma[i][j] = mat[i][j] + mat2[i][j];
+            			result[i][j] = mat[i][j] + mat2[i][j];
         				}
 				}
 				
-				printMatrix(r,c,suma);
+				printMatrix(r,c,result);
 			}
 			else {
 				printf("Matrix size error");
@@ -38,16 +52,15 @@ void matrix_op(int r, int c, int r2, int c2, double mat[r][c], double mat2[r2][c
 			
 		case '-':
 			if (r==r2 && c==c2) {
-				double sub[r][c];
 
 				int i,j;
 	 			for (i = 0; i < r; i++) {
         			for (j = 0; j < c; j++) {
-            			sub[i][j] = mat[i][j] - mat2[i][j];
+            			result[i][j] = mat[i][j] - mat2[i][j];
         				}
 				}
 				
-				printMatrix(r,c,sub);
+				printMatrix(r,c,result);
 			}
 			else {
 				printf("Matrix size error");
@@ -56,20 +69,19 @@ void matrix_op(int r, int c, int r2, int c2, double mat[r][c], double mat2[r2][c
 			
 		case '*':
 			if ((r==r2 && c==c2) || (r2==c)) {
-				double mul[r2][c2];
 				int i,j,k = 0;
 
 				for (i = 0; i < r2; i++) {
 	    			for (j = 0; j < c2; j++) {
-	        			mul[i][j] = 0;
+	        			result[i][j] = 0;
 	        
 	        			for (k = 0; k < r2; k++) {
-        					mul[i][j] += mat[i][k]*mat2[k][j];
+        					result[i][j] += mat[i][k]*mat2[k][j];
         				}
 	    			}
     			}
 				
-				printMatrix(r,c2,mul);
+				printMatrix(r,c2,result);
 			}
 			else {
 				printf("Matrix size error");
@@ -79,14 +91,9 @@ void matrix_op(int r, int c, int r2, int c2, double mat[r][c], double mat2[r2][c
 		default:
 			printf("Not an operation\n");
 
-		}
 	}
-void free_matrix(double** matrix, int n) {
-	int i;
-    for (i = 0; i < n; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
+	
+	free_matrix(result, r2);
 }
-
+	
 
