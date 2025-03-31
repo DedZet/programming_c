100e0838c22c5add644ae5bfb41fc02b72c099e3
DedZet <51733299+DedZet@users.noreply.github.com>
2025-03-31 17:04:16 +1000

    Update matrix.h

--- labs/lab5/grib/matrix.h
+++ labs/lab5/grib/matrix.h
@@ -1,5 +1,3 @@
-void free_matrix(double** matrix, int n);
-
 void printMatrix(int row, int col, double m[row][col]);
 
 void matrix_op(int r, int c, int r2, int c2, double mat[r][c], double mat2[r2][c2], char op);
