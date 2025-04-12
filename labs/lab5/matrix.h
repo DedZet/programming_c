986b0576d95134609a3a6c6482f0209f4d2ffe06
DedZet <51733299+DedZet@users.noreply.github.com>
2025-04-12 12:35:58 +1000

    Update matrix.h

--- labs/lab5/matrix.h
+++ labs/lab5/matrix.h
@@ -2,4 +2,4 @@ void free_matrix(double** matrix, int n);
 
 void printMatrix(int row, int col, double** m);
 
-void matrix_op(int r, int c, int r2, int c2, double** mat, double** mat2, char op);	
+double** matrix_op(int r, int c, int r2, int c2, double** mat, double** mat2, char op);	
