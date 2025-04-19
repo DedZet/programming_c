bd82ca38374835364b73dc9ce4e91ae2d4ca93d2
DedZet <51733299+DedZet@users.noreply.github.com>
2025-04-19 11:26:35 +1000

    Update main.c

--- labs/lab8/main.c
+++ labs/lab8/main.c
@@ -1,15 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef struct {
+	char name[32];
+	char surname[32];
+	int year;
+	char sex;
+	float height;
+} Human;
+
+int compare(Human a, Human b)
+{
+    return a.year - b.year;
+}
 
 int main(int argc, char *argv[]) {
 	
+	SetConsoleOutputCP(CP_UTF8);
+	
 	FILE *names = fopen("names.txt","r");
 	
+	FILE *sorted_year = fopen("out_year.txt","w");
+	FILE *sorted_names = fopen("out_names.txt","w");
+	FILE *sorted_surnames = fopen("out_surnames.txt","w");
+	FILE *sorted_sex = fopen("out_sex.txt","w");
+	FILE *sorted_height = fopen("out_height.txt","w");
+		
 	if (names == NULL) {
 		printf("File opening error");
+		fclose(names);
 	}
+	
+	int year;
+	char line[256], name[32], surname[32];
+	char sex;
+	float height;
+	
+	while (putchar(names) != EOF) {
+	
+            if (fscanf(names, "%s", name) == 1) {
+                //fprintf(sorted_sex, "%s %s %d %c %lf", name, surname, year, sex, height);
+                printf("%s", name);
+                
+                //printf("%s %s %d %c %lf\n", name, surname, year, sex, height);
+        }
+}
+	
+	//qsort();
+	
+	
 	
 	return 0;
 }
