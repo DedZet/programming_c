be36071b3b5dcdeb79e5b9a9351f6fff152397c5
DedZet <51733299+DedZet@users.noreply.github.com>
2025-04-30 11:57:37 +1000

    Update main.c

--- labs/lab6/main.c
+++ labs/lab6/main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
     while (fgets(line, sizeof(line), namedb))
     {
         if (sscanf(line, "%s %s %s %d", surname, name, surname2, &year) == 4) {
-            if (year > 1980) {
+            if (year > 1980 && name[0] == 'À') {
                 fprintf(output, "%s %s %s %d\n", surname, name, surname2, year);
                 printf("%s %s %s %d\n", surname, name, surname2, year);
             }
