e737bf8f151fb85fc4abff542c7faf0c3a510a08
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-01 16:54:20 +1000

    Update main.c

--- labs/lab8/main.c
+++ labs/lab8/main.c
@@ -30,25 +30,25 @@ void rw_file(FILE *filename, Human hums[]) {
 
 ///////////////////// SORT FUNCS //////////////////////////////
 
-void bsort_year(Human hum[], int size) {
-    int i,j;
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (hum[j].year > hum[j + 1].year)
-                swap(hum, j, j + 1);
-        }
-    }
-}
-
-void bsort_height(Human hum[], int size) {
-    int i,j;
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (hum[j].height > hum[j + 1].height)
-                swap(hum, j, j + 1);
-        }
-    }
-}
+//void bsort_year(Human hum[], int size) {
+//    int i,j;
+//    for (i = 0; i < size - 1; i++) {
+//        for (j = 0; j < size - i - 1; j++) {
+//            if (hum[j].year > hum[j + 1].year)
+//                swap(hum, j, j + 1);
+//        }
+//    }
+//}
+//
+//void bsort_height(Human hum[], int size) {
+//    int i,j;
+//    for (i = 0; i < size - 1; i++) {
+//        for (j = 0; j < size - i - 1; j++) {
+//            if (hum[j].height > hum[j + 1].height)
+//                swap(hum, j, j + 1);
+//        }
+//    }
+//}
 
 int compare_name(Human a, Human b) {
 	return strcmp(a.name, b.name);
@@ -56,6 +56,12 @@ int compare_name(Human a, Human b) {
 
 int compare_surname(Human a, Human b) {
 	return strcmp(a.surname, b.surname);
+}
+
+int compare_year(Human a, Human b) {
+    if (a.year < a.year) return -1;
+    if (a.year > a.year) return 1;
+    if (a.year == a.year) return 0;
 }
 
 int compare_genf(Human a, Human b) {
@@ -70,24 +76,89 @@ int compare_genm(Human a, Human b) {
 	if( a.gender == b.gender ) return 0;
 }
 
+int compare_height(Human a, Human b) {
+    if (a.height < a.height) return -1;
+    if (a.height > a.height) return 1;
+    if (a.height == a.height) return 0;
+}
+
+///////////////////////////////////////////////////////////////
+
+char chose[3];
+int combo_compare(Human a, Human b) {
+	
+	int result = 0;
+	char field1 = chose[0];
+	char field2 = chose[1];
+	
+	if (field1 == 'n')
+        result = compare_name(a, b);
+        
+    else if (field1 == 's')
+    	result = compare_surname(a, b);
+    	
+    else if (field1 == 'f')
+        result = compare_genf(a, b);
+        
+    else if (field1 == 'm')
+        result = compare_genm(a, b);
+        
+    else if (field1 == 'y')
+        result = compare_year(a, b);
+            
+    else if (field1 == 'h')
+        result = compare_height(a, b);
+        
+    // field 2
+    
+    if (result == 0) {
+    	
+        if (field2 == 'n')
+            result = compare_name(a, b);
+            
+        else if (field2 == 's')
+            result = compare_surname(a, b);
+            
+        else if (field2 == 'f') 
+            result = compare_genf(a, b);
+            
+        else if (field2 == 'm')
+            result = compare_genm(a, b);
+            
+        else if (field2 == 'y') 
+            result = compare_year(a, b);
+            
+        else if (field2 == 'h')
+            result = compare_height(a, b);
+
+	}	
+//
+//	else {
+//			printf("not an operation");
+//			return result;
+//		}
+		
+	return result;
+}
+
+
 ///////////////////////////////////////////////////////////////
 	
 Human *operations(Human hum[], char chose[]) {
 	
 	Human buffer[GLSIZE];
+	memcpy(buffer, hum, sizeof(hum));
 	
 	///////////// NOT COMBINED FILTERS /////////////
 	
-	if (strncmp(chose, "nn", 2) == 0) {  // name // NF test
-		qsort(hum, GLSIZE, sizeof(Human), compare_genf);
+	if (strncmp(chose, "nn", 2) == 0)   // name
 		qsort(hum, GLSIZE, sizeof(Human), compare_name);
-	}
 	
 	else if (strncmp(chose, "ss", 2) == 0) // surname
 		qsort(hum, GLSIZE, sizeof(Human), compare_surname);
 		
 	else if (strncmp(chose, "yy", 2) == 0) // year
-		bsort_year(hum, GLSIZE);
+		qsort(hum, GLSIZE, sizeof(Human), compare_year);
 		
 	else if (strncmp(chose, "ff", 2) == 0) // gender f
 		qsort(hum, GLSIZE, sizeof(Human), compare_genf);
@@ -96,29 +167,16 @@ Human *operations(Human hum[], char chose[]) {
 		qsort(hum, GLSIZE, sizeof(Human), compare_genm);
 		
 	else if (strncmp(chose, "hh", 2) == 0) // height
-		bsort_height(hum, GLSIZE);
+		qsort(hum, GLSIZE, sizeof(Human), compare_height);
 		
 		/////////////// COMBINED FILTERS ///////////////
 		
-		//if (chose)
-		
-		
-		
-		
-		
-		
-		
+	else qsort(hum, GLSIZE, sizeof(Human), combo_compare);
 		
 		////////////////// RETURNS ///////////////////
 		
-		else {
-			printf("not an operation");
-			return NULL;
-		}
-		
 	return hum;
 }
-	
 
 int main(int argc, char *argv[]) {
 	
@@ -126,7 +184,7 @@ int main(int argc, char *argv[]) {
 	
     Human humans[GLSIZE];
     Human sorted[GLSIZE];
-    char chose[3];
+    
 	
 	FILE *namesfile = fopen("names.txt","r");
 		
