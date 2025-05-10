a5fad970d9d935b1ad5390d2bffdd9c515230dcd
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-10 12:25:17 +1000

    Update main.c

--- labs/lab10/main.c
+++ labs/lab10/main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define FIND_MAX(arr, size) ({ \
 	int max = arr[0]; \
 	int i; \
-	for (i = 1; i < size; ++i) { \
+	for (i = 0; i < size+1; ++i) { \
         if (arr[i] > max) { \
             max = arr[i]; \
         } \
     } \
     max; \
+})
+
+#define FIND_AVERAGE(arr, size) ({ \
+	int sum = 0; \
+	int i; \
+	for (i = 0; i < size+1; i++) { \
+		sum = sum + arr[i]; \
+	} \
+	double result = sum / (size+1); \
+	result; \
 })
 
 int main(int argc, char *argv[]) {
@@ -23,8 +34,11 @@ int main(int argc, char *argv[]) {
         count++;
     }
     
-    int result = FIND_MAX(numbers, count);
-    printf("Max: %d", result);
+    int result_max = FIND_MAX(numbers, count);
+    int result_avr = FIND_AVERAGE(numbers, count);
+    
+    printf("Max: %d\nAverage: %d", result_max, result_avr);
+    
     
 	return 0;
 }
