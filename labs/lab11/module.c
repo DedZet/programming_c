653e239a5addce55f66aae80746f9b85e6806364
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-13 11:46:53 +1000

    Update module.c

--- labs/lab11/module.c
+++ labs/lab11/module.c
@@ -1,48 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
-int find_sum(int num, ...) {
-	
-	int sum = 0;
-	va_list list;
-	va_start(list, num);
-	
-	while (num) {
-		
-		sum += va_arg(list, int);
-		--num;
+void swap(int arr[], int i) {
+    int temp = arr[i];
+    arr[i] = arr[i+1];
+    arr[i+1] = temp;
+}
+
+int find_sum(int count, ...) {
+    va_list args;
+    int result = 0;
+    
+    va_start(args, count);
+    
+    int i;
+    for (i = 0; i < count; i++) result += va_arg(args, int);
+        
+    va_end(args);
+    return result;
+}
+
+int find_max(int count, ...) {
+    va_list args;
+    int result[count];
+    
+    va_start(args, count);
+    
+    
+    int i, j;
+    for(i = 0; i < count; i++) {
+        result[i] = va_arg(args, int);
+    }
+    
+    va_end(args);
+    
+    for (i = 0; i < count - 1; i++) {
+    	for (j = 0; j < count - i - 1; j++) {
+    		if (result[j] > result[j + 1]) {
+    			swap(result, j);
+			}
+		}
 	}
 	
-	va_end(list);
-    return sum;
-}
-
-int find_max(int arr[], int size) {
-	int max = arr[0];
-	int i;
-	for (i = 0; i < size+1; ++i) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-    return max; 
-}
-
-int find_min(int arr[], int size) {
-	int min = arr[0];
-	int i;
-	for (i = 0; i < size+1; i++) {
-        if (arr[i] < min) {
-            min = arr[i]; 
-        } 
-    } 
-    return min; 
-}
-
-int find_avr(int arr[], int size) {
-	int sum = find_sum(arr, size);
-	return sum/(size+1);
-}
-
+	va_end(args);
 	
+    return result[count-1];
+}
+
+int find_min(int count, ...) {
+    va_list args;
+    int result[count];
+    
+    va_start(args, count);
+    
+    int i, j;
+    for(i = 0; i < count; i++) {
+        result[i] = va_arg(args, int);
+    }
+    
+    va_end(args);
+    
+    for (i = 0; i < count - 1; i++) {
+    	for (j = 0; j < count - i - 1; j++) {
+    		if (result[j] > result[j + 1]) {
+    			swap(result, j);
+			}
+		}
+	}
+	
+    return result[0];
+}
+
+int find_avr(int count, ...) {
+    va_list args;
+    int result = 0;
+    
+    va_start(args, count);
+    
+    int i;
+    for (i = 0; i < count; i++) result += va_arg(args, int);
+
+    va_end(args);
+    
+    return result / count;
+}
+
