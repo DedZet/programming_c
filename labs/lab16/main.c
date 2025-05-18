657a99ae5a031b9bca7465b6652c8e1475979db8
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-18 20:11:02 +1000

    Create main.c

---
+++ labs/lab16/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int count_bits_long(long num) {
+	
+    int count = 0;
+    unsigned long unum = (unsigned long)num;
+    
+    while (unum != 0) {
+        count += unum & 1; 
+        unum >>= 1;
+    }
+    return count;
+}
+
+int count_bits_double(double num) {
+    int count = 0;
+    uint64_t *ptr = (uint64_t *)&num;
+    uint64_t bits = *ptr;
+    
+    int i;
+    for (i = 0; i < 64; i++) {
+        count += (bits >> i) & 1;
+    }
+    return count;
+}
+
+int main() {
+	
+    long numL;
+    double numD;
+
+    printf("Enter long: ");
+    scanf("%ld", &numL);
+    printf("Bit count: %d\n", count_bits_long(numL));
+
+    printf("\nEnter double: ");
+    scanf("%lf", &numD);
+    printf("Bit count: %d\n", count_bits_double(numD));
+
+    return 0;
+}
