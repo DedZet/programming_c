de06ee4779b8f0a4d91283dd2d7a523f1357a58f
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-21 22:22:47 +1000

    Update calendar.c

--- labs/lab12/calendar.c
+++ labs/lab12/calendar.c
@@ -7,16 +7,12 @@
 int days_in_month[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
 char *format(struct tm *p, char *format) {
-	time_t t;
-	char s[32];
 	
-	t = time(NULL);
-	p = gmtime(&t);
+	char s[32];
 	strftime(s, 32, format, p);
 
 	return s;
 }
-
 
 char *months[]= {
 	" ",
@@ -34,15 +30,6 @@ char *months[]= {
 	"December"
 };
 
-char *dayname[]= {
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
-	"Sunday" 
-};
 
 int set_daycode(int year) {
 	
@@ -69,10 +56,10 @@ int is_leap(int year) {
 	}
 }
 
-void print_calendar(stuct int year, int daycode) {
+void print_calendar(int daycode) {
 	
 	int month, day;
-	for ( month = 1; month <= 12; month++ )
+	for (month = 1; month <= 12; month++ )
 	{
 		printf("\n-------------%s-------------", months[month]);
 		printf("\nSun  Mon  Tue  Wed  Thu  Fri  Sat\n" );
@@ -90,9 +77,10 @@ void print_calendar(stuct int year, int daycode) {
 	}
 }
 
-void print_month(int year, int mon, int daycode)
-{
+void print_month(int mon, int daycode) {
+	
 	int month, day;
+	
 	for (month = 1; month <= 12; month++ )
 	{
 			
