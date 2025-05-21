621a38ff0ce25859df0da779e3c91ebd1b63f2a0
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-21 18:21:15 +1000

    Update strftime.c

--- labs/lab12/strftime.c
+++ labs/lab12/strftime.c
@@ -2,7 +2,7 @@ int main() {
 	time_t t;
 	struct tm *p;
 	char s[80];
-	char *format = "%A %B %Y";
+	char *format = "%A %B %Y"; // Saturday April 2019
 	t = time(NULL);
 	p = gmtime(&t);
 	strftime(s,80,format,p);
