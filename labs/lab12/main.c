70f0c6f924fbf56304ac0626b065ca1983a6757b
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-21 22:00:16 +1000

    Update main.c

--- labs/lab12/main.c
+++ labs/lab12/main.c
@@ -18,9 +18,9 @@ int main(int argc, char *argv[]) {
 		strncpy(day, input+8, 2);  day[2] = '\0';
 		
 		struct tm t;
-		t.tm_year = atoi(year) - 1900;		// 2025
-		t.tm_mon = atoi(month) - 1;			// 05
-		t.tm_mday = atoi(day);				// 21
+		t.tm_year = atoi(year) - 1900;
+		t.tm_mon = atoi(month) - 1;	
+		t.tm_mday = atoi(day);
 		
 //		t.tm_hour = 0;
 //		t.tm_min = 0;
@@ -38,27 +38,30 @@ int main(int argc, char *argv[]) {
 		strncpy(year, input+2, 2); year[4] = '\0';
 		strncpy(month, input+5, 2); month[2] = '\0';
 		
-		int inyear = atoi(year);	
+		struct tm t;
+		t.tm_year = atoi(year) - 1900;
+		t.tm_mon = atoi(month);
+		
+		mktime(&t);
 
-		daycode = set_daycode(inyear);
-		is_leap(inyear);
+		daycode = set_daycode(t.tm_year);
+		is_leap(t.tm_year);
 	
-		print_month(inyear, atoi(month), daycode);
+		print_month(t.tm_mon, daycode);
 		printf("\n");
 		
-		daycode = set_daycode(atoi(year));
+		daycode = set_daycode(t.tm_year);
 	}
 	
 	else if (len == 4) { // yyyy
 	
 		strncpy(year, input+2, 2); year[4] = '\0';
 		
-		int daycode, leapyear;
 		int inyear = atoi(year);
-		
-		daycode = set_daycode(inyear);
+		int daycode = set_daycode(inyear);
 		is_leap(inyear);
-		print_calendar(inyear, daycode);
+		
+		print_calendar(daycode);
 	}
 	
 	else if ((strcmp(input, "now")) == 0) {
