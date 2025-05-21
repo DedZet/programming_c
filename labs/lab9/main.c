816036fc8c416989cc69ca91a755a760cf4f959e
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-21 21:05:43 +1000

    Update main.c

--- labs/lab9/main.c
+++ labs/lab9/main.c
@@ -1,66 +1,50 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+double eval(char *exp) {
+	
+	double result = 0;
+    int sign = 1;
+    
+    while (*exp != "\0") {
+        if (*exp == ' ') {
+			exp++;
+			continue;
+		}
+        
+        if (*exp == '+') {
+            sign = 1;
+            exp++;
+        }
+        
+        else if (*exp == '-') {
+            sign = -1;
+            exp++;
+        }
+        
+        char *end;
+        double num = strtod(exp, &end);
+        
+        if (exp == end) {
+        	break;
+		}
+        
+        result += sign * num;
+        exp = end;
+        sign = 1;  
+    }
+    return result;
+}
 
 int main() {
 	
-    char exp[256];
-    
+    char input[128];
     printf("Enter expression: ");
-    scanf("%256s", exp);
+    scanf("%s", input);
     
-    double result = 0;
-    double num = 0;
-    char op = '+';
-
-	int i = 0;
-    while (exp[i] != '\0' && exp[i] != '\n') {
-        while (exp[i] == ' ') i++; // string skip 
-        
-        if (isdigit(exp[i]) || exp[i] == '.') {
-            double temp = 0;
-            int dot = 0;
-            double div = 1;
-            
-            while (isdigit(exp[i]) || exp[i] == '.') {
-                if (exp[i] == '.') {
-                	if (dot) {
-						printf("NaN\n");
-                		return 1;
-                	}
-                
-                dot = 1;
-            	}
-                
-				else {
-                    if (dot) {
-                        div *= 10;
-                        temp = temp * 10 + (exp[i] - '0');
-                    }
-					else {
-                        temp = temp * 10 + (exp[i] - '0');
-                    }
-                }
-                i++;
-            }
-            
-            if (dot) temp /= div;
-            num = temp;
-        }
-        
-
-        if (exp[i] == '+' || exp[i] == '-' || exp[i] == '\0') {
-            if (op == '+') result += num;
-            else if (op == '-') result -= num;
-             
-            if (exp[i] != '\0') {
-                op = exp[i];
-                i++;
-            }
-            num = 0;
-
-    	}
-	}
-    
+    double result = eval(input);
     printf("Result: %.2lf\n", result);
+    
     return 0;
 }
