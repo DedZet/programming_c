da60d1f8f3e6fc55c19ea0b1e935c816f57e5055
DedZet <51733299+DedZet@users.noreply.github.com>
2025-05-22 15:24:20 +1000

    Create X.c

---
+++ vas labs/Lab7/X.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <windows.h>
+
+typedef struct  {
+    char name[32];
+    char surname[32];
+    int year;
+    
+} Human;
+
+void swap(Human hum[], int i, int j) {
+    Human temp = hum[i];
+    hum[i] = hum[j];
+    hum[j] = temp;
+}
+
+void bubbleSort(Human hum[], int size) {
+    int i,j;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (hum[j].year > hum[j + 1].year)
+				swap(hum, j, j + 1);
+        }
+    }
+}
+	
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	
+	FILE *inputfile = fopen("input.txt", "r");
+	
+	if (inputfile == NULL) {
+		printf("file opening error");
+		return 1;
+	}
+	
+	int count = 1;
+    char c;
+	
+	while ((c = fgetc(inputfile)) != EOF)
+    {
+        if ( c == '\n')
+        {
+            count++;
+        }
+    }
+    
+	Human input[count];
+    Human sorted[count];
+    
+    rewind(inputfile);
+
+    int i;
+    for (i = 0; i < count; i++) {
+        fscanf(inputfile, "%s %s %d", input[i].name, input[i].surname, &input[i].year);
+    }
+    
+    fclose(inputfile);
+    
+    memcpy(sorted, input, sizeof(input));
+
+	bubbleSort(sorted, count);
+	
+    printf("Sorted humans:\n");
+    
+    for (i = 0; i < count; i++) {
+        printf("%s %s, %d\n", sorted[i].name, sorted[i].surname, sorted[i].year);
+    }
+
+    return 0;
+}
+
